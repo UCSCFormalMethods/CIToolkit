d14409fa1186f9aa06e1b8ef883710f14847cc46
Eric Vin <ericvin@outlook.com>
2022-01-07 13:30:06 -0800

    Fixed accepting state

--- experiments/gridworld.py
+++ experiments/gridworld.py
@@ -338,7 +338,7 @@ def make_dfa_wrapper(input_data):
 
     label_num, cost_val = class_key
 
-    accepting_states = {state_map[(start_loc[0], start_loc[1], cost_val, (1,1,1,1), label_num)]}
+    accepting_states = {state_map[(end[0], end[1], cost_val, (1,1,1,1), label_num)]}
 
     new_dfa = Dfa(alphabet, states, accepting_states, start_state, transitions).minimize()
 
